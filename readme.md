b63ed26a212af1444a3499fd2acc48ee03c4537c
Amir Kadyrov <Amir.Kadyrov@ringcentral.com>
2023-05-20 14:00:53 +0400

    read from file

---
+++ readme.md
@@ -0,0 +1,14 @@
+#### Compile
+```
+cmake .
+make
+```
+#### Run
+```
+Usage: dict input_file [output_file]
+If output_file is not specified, std::cout will be used for output
+```
+
+```
+./freq text/002.txt output.txt
+```
--- src/freq.cpp
+++ src/freq.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
-#include <sstream>
+#include <vector>
 #include <cstring>
 #include <unordered_map>
 #include <algorithm>
+#include <fstream>
+#include <stdexcept>
 
 using wordsMap = std::unordered_map<std::string, unsigned>;
 
@@ -73,11 +75,46 @@ void map2Stream(const wordsMap& wMap, std::ostream& os) {
   }
 }
 
-int main() {
-  std::stringstream ss;
-  ss << "The time has come, the Walrus said,\nto talk of many things...";
-  auto mp = readStream(ss);
-  map2Stream(mp, std::cout);
+void checkArguments(int argc, char *argv[], std::ifstream& ifs, std::ofstream& ofs) {
+  const char* usage = 
+    "Usage: dict input_file [output_file]\n" \
+    "If output_file is not specified, std::cout will be used for output";
+
+  if(argc < 2 || argc > 3) {
+    std::cout << usage << std::endl;
+    if(argc < 2) {
+      throw std::invalid_argument("Error: input_file is not specified");
+    }
+  }
+
+  std::ifstream ifile(argv[1]);
+  if(!ifile) {
+    throw std::invalid_argument(std::string("Cannot open file ") + argv[1]);
+  }
+
+  if( argc == 3 ){
+    std::ofstream ofile(argv[2]);
+    if(!ofile) {
+      throw std::invalid_argument(std::string("Cannot create file ") + argv[2]);
+    }
+    ofs = std::move(ofile);
+  }
+
+  ifs = std::move(ifile);
+}
+
+int main(int argc, char *argv[]) {
+  try {
+    std::ifstream ifs;
+    std::ofstream ofs;
+    checkArguments(argc, argv, ifs, ofs);
+    auto mp = readStream(ifs);
+    map2Stream(mp, ofs.is_open() ? ofs : std::cout);
+  }
+  catch(const std::exception& ex){
+    std::cout << ex.what();
+    return 1;
+  }
 
   return 0;
 }
